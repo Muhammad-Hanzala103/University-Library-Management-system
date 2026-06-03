3b6b76f85341331e44cc56285238ee0c63708cef
Muhammad-Hanzala103 <hani75384@gmail.com>
2026-06-04 00:57:08 +0500

    feat: add BookFormWindow and BookCatalogView components to the desktop application

--- KicsitLibrary.Tests/obj/Debug/net8.0/KicsitLibrary.Tests.AssemblyInfo.cs
+++ KicsitLibrary.Tests/obj/Debug/net8.0/KicsitLibrary.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KicsitLibrary.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc567ac9218eb907ae39b7e8f4fe4bb242da69be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a0ae55378b2391ab4fe829b409fac950602a980")]
 [assembly: System.Reflection.AssemblyProductAttribute("KicsitLibrary.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KicsitLibrary.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
